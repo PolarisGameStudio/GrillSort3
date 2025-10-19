c553f757192646723ed649a74297b3141cb10740
datlt <datlt@sonat.vn>
2025-10-19 19:21:51 +0700

    [Add] Anim widget

---
+++ Assets/_MyGame/SkewerJam/Scripts/UI/UILoopAnimation.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using SonatFramework.Scripts.Utils;
+using Spine.Unity;
+using UnityEngine;
+
+namespace MyGame.UI
+{
+    [RequireComponent(typeof(SkeletonGraphic))]
+    public class UILoopAnimation : MonoBehaviour
+    {
+        [SerializeField] private string animationName1;
+        [SerializeField] private string animationName2;
+
+        [SerializeField] private float delay;
+        private SkeletonGraphic skeletonGraphic;
+
+        private void OnEnable()
+        {
+            skeletonGraphic = GetComponent<SkeletonGraphic>();
+
+
+            LoopAnimation();
+
+        }
+
+        private void LoopAnimation()
+        {
+            skeletonGraphic.AnimationState.SetAnimation(0, animationName1, true);
+            SonatUtils.DelayCall(delay, () =>
+            {
+                skeletonGraphic.AnimationState.SetAnimation(0, animationName2, false).Complete += (track) =>
+                {
+                    LoopAnimation();
+                };
+            }, this);
+        }
+
+
+    }
+}
